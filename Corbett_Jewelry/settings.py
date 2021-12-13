2a1318feab498e179d8c4520d7254b5001e5adcc
Admin <corbettjewelry991.com>
2021-12-13 19:14:05 +0545

    Needed Debug

--- Corbett_Jewelry/settings.py
+++ Corbett_Jewelry/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'nupe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.105','127.0.0.1']
 
