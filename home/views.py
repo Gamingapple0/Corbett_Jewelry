09a5b94d8035909d4106dae2048cd88f26f22d1e
Gamingapple <madhikarmianshu@gmail.com>
2022-02-07 07:05:22 +0545

    Update views.py
    Accidentally unfixed the views problem

--- home/views.py
+++ home/views.py
@@ -2,46 +2,46 @@ from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Product, Images, Beads
 from django.core.mail import send_mail
 
-products = Product.objects.all()
-max_products_per_page = 9
-home_page = products[:max_products_per_page]
-divided_product_list = []
-first = max_products_per_page
-last = max_products_per_page + max_products_per_page
-curr_page = 1
-for i in range(max_products_per_page,len(products),max_products_per_page):
-    divided_product_list.append(products[first:last])
-    first = last
-    last += max_products_per_page
+# products = Product.objects.all()
+# max_products_per_page = 9
+# home_page = products[:max_products_per_page]
+# divided_product_list = []
+# first = max_products_per_page
+# last = max_products_per_page + max_products_per_page
+# curr_page = 1
+# for i in range(max_products_per_page,len(products),max_products_per_page):
+#     divided_product_list.append(products[first:last])
+#     first = last
+#     last += max_products_per_page
 
 
-# def home(request):
-#     return render(request, 'index.html')
-
-# def nxt_pg(request):
-#     return render(request, 'index.html')
-
-# def detail_view(request):
-#     return render(request, 'index.html')
-    
 def home(request):
-    return render(request, 'index.html', {'products' : home_page,'nxt':1,'prev':len(divided_product_list)})
+    return render(request, 'index.html')
 
-def nxt_pg(request,num):
-    if num > len(divided_product_list) or num == 0:
-        return home(request)
-    elif num < 0:
-        num = len(divided_product_list)
-    return render(request, 'index.html', {'products': divided_product_list[num-1],'nxt':num+1,'prev':num-1})
+def nxt_pg(request):
+    return render(request, 'index.html')
+
+def detail_view(request):
+    return render(request, 'index.html')
+    
+# def home(request):
+#     return render(request, 'index.html', {'products' : home_page,'nxt':1,'prev':len(divided_product_list)})
+
+# def nxt_pg(request,num):
+#     if num > len(divided_product_list) or num == 0:
+#         return home(request)
+#     elif num < 0:
+#         num = len(divided_product_list)
+#     return render(request, 'index.html', {'products': divided_product_list[num-1],'nxt':num+1,'prev':num-1})
 
 
-def detail_view(request,slug):
-    product = get_object_or_404(Product,slug=slug)
-    images = Images.objects.filter(product=product)
-    return render(request, 'details.html', {
-        'product': product,
-        'images': images
-    })
+# def detail_view(request,slug):
+#     product = get_object_or_404(Product,slug=slug)
+#     images = Images.objects.filter(product=product)
+#     return render(request, 'details.html', {
+#         'product': product,
+#         'images': images
+#     })
 
 def purchased(request, slug):
     if request.method == 'POST':
